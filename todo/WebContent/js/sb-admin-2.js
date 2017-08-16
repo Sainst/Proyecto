6aab569f363c88f93fe305800336891e7b88478f
Sainst <casesa97@gmail.com>
2017-08-16 15:02:25 -0500

    edicion
    verificacion login

--- todo/WebContent/js/sb-admin-2.js
+++ todo/WebContent/js/sb-admin-2.js
@@ -7,6 +7,9 @@ $(function() {
     $('#side-menu').metisMenu();
 });
 
+	 
+
+
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
@@ -45,3 +48,4 @@ $(function() {
         }
     }
 });
+
